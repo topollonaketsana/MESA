cedb568e586fba0b1db010e6a01ea7732b027b47
topollonaketsana <topollomaketsana60@gmail.com>
2026-03-08 16:29:52 +0200

    update analyze.py script

--- tutorial/1.semiconvection/analyze.py
+++ tutorial/1.semiconvection/analyze.py
@@ -1,4 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+# extract data from logs
+d1= './LOGS/pgstar.dat'
+d2= './LOGS/history.data'
+d3= './LOGS/profiles.index'
+
+
+# We want to open each file appropriately according to their format
+
 
 
 
